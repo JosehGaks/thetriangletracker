696eba67917933301d95aa8096ec7c65721bddcb
JosehGaks <josehwilddog@gmail.com>
2019-02-17 04:14:33 -0500

    add alert methods to output the result

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function submit(){
 
  if ((side1+side2>side3) && (side1+side3>side2) && (side2 + side3 > side1)){
    if(side1===side2 && side2===side3){
-     alert("The triangle you entered is\n\n.........\n..........\n.........\n..........\n.........\n     equilateral");
+     alert("The triangle you entered is\n\n.........\n..........\n.........\n..........\n.........\n equilateral");
       }
    else if (side1===side2 || side1===side3 || side2===side3) {
      alert("Isosceles");}
