988b62f1a0189e7f093af9dc41c2ba657e8a7c38
JosehGaks <josehwilddog@gmail.com>
2019-02-17 05:08:30 -0500

    add tables in the readme

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,19 @@ function submit(){
   var side2=parseInt(document.getElementById("secondside").value);
   var side3=parseInt(document.getElementById("thirdside").value);
 
- if ((side1+side2>side3) && (side1+side3>side2) && (side2 + side3 > side1)){
-   if(side1===side2 && side2===side3){
-     alert("The triangle you entered is\n\n.........\n..........\n.........\n..........\n.........\n equilateral");
+  if ((side1+side2>side3) && (side1+side3>side2) && (side2 + side3 > side1)){
+    if(side1===side2 && side2===side3){
+      alert("The triangle you entered is\n\n.........\n..........\n.........\n..........\n.........\n equilateral");
+    }
+    else if (side1===side2 || side1===side3 || side2===side3) {
+      alert("The triangle you entered is\n\n.........\n..........\n.........\n..........\n.........\n Isosceles");}
+      else {
+        alert("The triangle you entered is\n\n.........\n..........\n.........\n..........\n.........\n scalene");
       }
-   else if (side1===side2 || side1===side3 || side2===side3) {
-     alert("The triangle you entered is\n\n.........\n..........\n.........\n..........\n.........\n Isosceles");}
-   else {
-     alert("The triangle you entered is\n\n.........\n..........\n.........\n..........\n.........\n scalene");
-   }
-}
+    }
 
-else {
-  alert("oooops \n\n   :(  :(............\n\n\n\n\n\That's not a triangle!!!!!!");
-}
+    else {
+      alert("oooops \n\n   :(  :(............\n\n\n\n\n\That's not a triangle!!!!!!");
+    }
 
-}
+  }
